ff6be3bea1caf11a3ce32c578bb8472b783c0b1f
Philippe Gris <gris@clermont.in2p3.fr>
2022-03-29 15:16:37 +0200

    add a dir for useful scripts - ex: make_movie_from_img

---
+++ run_scripts/utils/make_movie_from_img.py
@@ -0,0 +1,33 @@
+import os
+from optparse import OptionParser
+
+parser = OptionParser()
+
+parser.add_option("--figDir", type="str", default='figures',
+                  help="fig directory [%default]")
+parser.add_option("--movieDir", type="str", default='movies',
+                  help="directory where movies will be placed[%default]")
+parser.add_option("--prefix", type="str", default='healpix36503',
+                  help="prefix for the figs to assemble as movie [%default]")
+parser.add_option("--extens", type="str", default='png',
+                  help="extension of the figures to assemble as a movie [%default]")
+parser.add_option("--rate", type=int, default=10,
+                  help="movie rate [%default]")
+opts, args = parser.parse_args()
+
+figDir = opts.figDir
+movDir = opts.movieDir
+prefix = opts.prefix
+extens = opts.extens
+rate = opts.rate
+
+if not os.path.exists(movDir):
+    os.mkdir(movDir)
+
+cmd = 'ffmpeg -v verbose -r {} -s 1920x1080 -f image2 -i {}/{}_%00d.{} -vcodec libx264 -crf 25  -pix_fmt yuv420p {}/{}.mp4 -y'.format(
+    rate, figDir, prefix, extens, movDir, prefix)
+
+print(cmd)
+
+
+os.system(cmd)
