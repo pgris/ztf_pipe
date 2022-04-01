b61f30821eedb8c8f29bf029150768ae638deb3e
Philippe Gris <gris@clermont.in2p3.fr>
2022-04-01 14:26:01 +0200

    some code cleaning

--- run_scripts/cadence/pixelize.py
+++ run_scripts/cadence/pixelize.py
@@ -8,11 +8,88 @@ from optparse import OptionParser
 import pandas as pd
 import numpy as np
 import healpy as hp
+import time
 
 
-def multipix(data, params={}, j=0, output_q=None):
+def sel_obs_sky(df_obs,
+                ra_min, ra_max, dec_min, dec_max,
+                width_ra, width_dec,
+                raCol='ra', decCol='dec', k=1.05):
     """
-    Function to perfom sky pixelizing
+    Method to select observations on a sky patch
+    It is mandatory to extand the area defined by (ra_min,ra_max,dec_min,dec_max)
+    to widths of quadrants.
+
+    Parameters
+    --------------
+    df_obs: pandas df
+      data to process
+    ra_min: float
+      min ra of the area
+    ra_max: float
+      max ra of the area
+   dec_min: float
+      min dec of the area
+    dec_max: float
+      max dec of the area
+    raCol: str, opt
+      ra col name in df_obs (default: ra)
+    decCol: str, opt
+      dec col name in df_obs (default: dec)
+    k: float, opt
+      security factor to grab all the data corresponding to the area (default: 1.05)
+
+    Returns
+    ----------
+    data inside the area (pandas df)
+
+    """
+    idx = df_obs[raCol] >= ra_min-k*width_ra
+    idx &= df_obs[raCol] < ra_max+k*width_ra
+    idx &= df_obs[decCol] >= dec_min-k*width_dec
+    idx &= df_obs[decCol] < dec_max+k*width_dec
+
+    return df_obs[idx]
+
+
+def obs_to_pixel(df_obs, nside, nproc, alignquads):
+    """
+    Method to project data (quadrants) on the sky to grab pixels
+
+    Parameters
+    --------------
+    df_obs: pandas df
+      data to process
+    nside: int
+      nside param for healpix
+    nproc: int
+      nproc for multiprocessing
+    alignquads: int
+      if alignquads was done or not
+
+    """
+    params = {}
+    params['nside'] = nside
+    params['raCol'] = 'ra'
+    params['decCol'] = 'dec'
+
+    if opts.alignquads:
+        params['raCol'] = 'ra_quad'
+        params['decCol'] = 'dec_quad'
+
+    # pixelize the data
+    time_ref = time.time()
+    print('to process', len(df_obs))
+    dfOut = multiproc(df_obs[:480000], params, multiobs_pixel, nproc)
+
+    print('processed', time.time()-time_ref)
+    dfOut.to_hdf(opts.outFile, key='obs')
+    #dfOut.to_parquet('df.parquet.gzip', compression='gzip')
+
+
+def multiobs_pixel(data, params={}, j=0, output_q=None):
+    """
+    Function to perfom sky pixelizing from data
     with the possibility to use multiprocessing
 
     Parameters
@@ -47,7 +124,29 @@ def multipix(data, params={}, j=0, output_q=None):
 
 
 def pixel_to_obs(healpixels, nside, data, width_ra, width_dec, raCol, decCol, nproc):
+    """
+    Method to get data corresponding to healpixels
 
+    Parameters
+    --------------
+    healpixels: list(int)
+      list of pixels to process
+    nside: int
+      nside for healpix
+    data: pandas df
+       data to process
+    width_ra: float
+      quadrant ra width
+    width_dec: float
+      quadrant dec width
+    raCol: str
+      ra col name
+     decCol: str
+      dec col name
+    nproc: int
+     number of procs for multiprocessing
+
+    """
     params['nside'] = nside
     params['data'] = data
     params['width_ra'] = width_ra
@@ -56,13 +155,31 @@ def pixel_to_obs(healpixels, nside, data, width_ra, width_dec, raCol, decCol, np
     params['decCol'] = decCol
 
     print('total to process per proc', len(healpixels)/nproc)
-    res = multiproc(healpixels[:400], params, multipixel_obs, nproc)
+    res = multiproc(healpixels, params, multipixel_obs, nproc)
 
     return res
 
 
 def multipixel_obs(ppix, params={}, j=0, output_q=None):
+    """
+    Method to get data corresponding to healpixels using multiprocessing
 
+    Parameters
+    --------------
+    ppix: list(int)
+      list of pixels to process
+    params: dict, opt
+      parameters to run (default: none)
+    j: int, opt
+      index for multiprocessing (default: 0)
+    output_q: multiprocessing queue, opt
+      default: None
+
+    Returns
+    ----------
+    pandas df of multiprocessed data
+
+    """
     nside = params['nside']
     df_obs = params['data']
     width_ra = params['width_ra']
@@ -91,19 +208,27 @@ def multipixel_obs(ppix, params={}, j=0, output_q=None):
         return dtot
 
 
-def get_min_max(dd, what):
-
-    return dd[what].min(), dd[what].max()
-
-
 def alignquads(df):
+    """
+    Method to align quadrants using multiprocessing
+
+    Parameters
+    --------------
+    df: pandas df
+       data to processed
+
+    Returns
+    ----------
+    pandas df of processed data
+
+    """
 
     pp = {}
     pp['obs'] = df
     # align quadrants
     dfo = multiproc(df['field'].unique(), pp, align_quad, opts.nproc)
-    print('aligned', len(dfo))
     dfo = dfo.sort_values(by=['time'])
+
     return dfo
 
 
@@ -135,19 +260,22 @@ logObs = params['logObs']
 print('loading', logObs)
 df_obs = pd.read_csv(logObs)
 
-ra_min, ra_max = get_min_max(df_obs, 'ra')
-dec_min, dec_max = get_min_max(df_obs, 'dec')
-delta_dec = dec_max-dec_min
-print(ra_min, ra_max, dec_min, dec_max)
-
-ra_min = 0.
-ra_max = 36.
+ra_min = opts.RAmin
+ra_max = opts.RAmax
+dec_min = opts.Decmin
+dec_max = opts.Decmax
 delta_ra = ra_max-ra_min
 
-width_ra = 0.854
-width_dec = 0.854
-idx = df_obs['ra'] >= ra_min-1.05*width_ra
-idx = df_obs['ra'] < ra_max+1.05*width_ra
+width_ra = opts.width_RA
+width_dec = opts.width_Dec
+
+df_obs = sel_obs_sky(df_obs, ra_min, ra_max, dec_min,
+                     dec_max, width_ra, width_dec, k=1.05)
+
+# re-align quadrant centers - if requested
+
+if opts.alignquads:
+    df_obs = alignquads(df_obs)
 
 """
 # get obs in this window
@@ -160,12 +288,8 @@ ppix = pixels(opts.nside, np.mean([ra_min, ra_max]), np.mean(
 print(ppix, len(ppix))
 """
 
-# re-align quadrant centers - if requested
-
-if opts.alignquads:
-    df_obs = alignquads(df_obs)
-
- # get pixels in this window
+# from pixels -> obs
+# get pixels in this window
 """
 ppix = pixels(opts.nside, np.mean([ra_min, ra_max]), np.mean(
     [dec_min, dec_max]), delta_ra, delta_dec)
@@ -173,33 +297,6 @@ ppix = pixels(opts.nside, np.mean([ra_min, ra_max]), np.mean(
 df = pixel_to_obs(ppix, opts.nside, df_obs, width_ra,
                   width_dec, 'ra_quad', 'dec_quad', opts.nproc)
 """
-"""
-pixel_to_obs = Pixel2Obs(opts.nside, df_obs, width_ra,
-                         width_dec, raCol='ra_quad', decCol='dec_quad')
-import time
-for healpix in ppix:
 
-    time_ref = time.time()
-    dp = pixel_to_obs(healpix)
-    print('pproc', healpix, time.time()-time_ref)
-"""
-
-
-params = {}
-params['nside'] = opts.nside
-params['raCol'] = 'ra'
-params['decCol'] = 'dec'
-
-if opts.alignquads:
-    params['raCol'] = 'ra_quad'
-    params['decCol'] = 'dec_quad'
-
-
-# pixelize the data
-import time
-time_ref = time.time()
-dfOut = multiproc(df_obs[:40000], params, multipix, opts.nproc)
-
-print('processed', time.time()-time_ref)
-#dfOut.to_hdf(opts.outFile, key='obs')
-dfOut.to_parquet('df.parquet.gzip', compression='gzip')
+# from obs -> pixels
+obs_to_pixel(df_obs, opts.nside, opts.nproc, opts.alignquads)
