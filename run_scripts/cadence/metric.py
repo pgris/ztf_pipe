fba44cffb5166921052d0107f4514415bee765d1
Philippe Gris <gris@clermont.in2p3.fr>
2022-04-25 15:48:51 +0200

    add a couple of parameters - nside and coadd_night

--- run_scripts/cadence/metric.py
+++ run_scripts/cadence/metric.py
@@ -17,6 +17,10 @@ parser.add_option('--metric', type=str, default='CadenceMetric',
                   help='metric name [%default]')
 parser.add_option('--nproc', type=int, default=8,
                   help='number of procs for multiprocessing [%default]')
+parser.add_option('--nside', type=int, default=128,
+                  help='nside healpix parameter [%default]')
+parser.add_option('--coadd_night', type=int, default=1,
+                  help='to perform coaddition per band and per night [%default]')
 
 opts, args = parser.parse_args()
 
@@ -26,10 +30,12 @@ outName = opts.outName
 output_dir = opts.output_dir
 metric_name = opts.metric
 nproc = opts.nproc
+nside = opts.nside
+coadd_night = opts.coadd_night
 
 df = pd.read_hdf('{}/{}'.format(input_dir, fileName))
 
-resdf = processMetric_multiproc(metric_name, df, nproc)
+resdf = processMetric_multiproc(metric_name, df, nproc, nside, coadd_night)
 
 checkDir(output_dir)
 fName = '{}/{}'.format(output_dir, outName)
