204c2a8f6d3cc9d719a64010c475cc8c64c69e34
pgris <gris@clermont.in2p3.fr>
2022-05-19 13:59:41 +0200

    add outputdirdimu as script parameter

--- batch_scripts/loop_simu.py
+++ batch_scripts/loop_simu.py
@@ -6,6 +6,7 @@ parser = OptionParser()
 
 parser.add_option('--obsDir', type=str, default='/sps/ztf/users/gris/pixelized_newformat',
                   help='dir with observations [%default]')
+parser.add_option('--outputDirSimu',type=str,default='/sps/ztf/users/gris/dataLC', help='output dir for simulation [/%default]')
 
 opts, args = parser.parse_args()
 
@@ -15,6 +16,7 @@ for fi in fis:
     obsFile = fi.split('/')[-1]
     cmd = 'python batch_scripts/patch_simu.py'
     cmd += ' --obsFile {}'.format(obsFile)
+    cmd += ' --outputDirSimu {}'.format(opts.outputDirSimu)
 
     print(cmd)
     os.system(cmd)
