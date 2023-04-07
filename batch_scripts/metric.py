adbab90363a789599accbe2796d59ea4ff713e71
pgris <gris@clermont.in2p3.fr>
2023-04-07 13:09:52 +0200

    add type_data parameter for the script

--- batch_scripts/metric.py
+++ batch_scripts/metric.py
@@ -14,13 +14,17 @@ parser.add_option('--metric', type=str, default='CadenceMetric',
                   help='metric name (CadenceMetric, RedMagMetric) [%default]')
 parser.add_option('--nproc', type=int, default=8,
                   help='number of procs for multiprocessing [%default]')
-
+parser.add_option('--nproc', type=int, default=8,
+                  help='number of procs for multiprocessing [%default]')
+parser.add_option('--type_data', type=str, default='pandas',
+                  help='type of the table in the meta data file (pandas or AstropyTable) [%default]')
 opts, args = parser.parse_args()
 
 input_dir = opts.input_dir
 output_dir = opts.output_dir
 metric_name = opts.metric
 nproc = opts.nproc
+type_data = opts.type_data
 
 checkDir(output_dir)
 
@@ -31,14 +35,16 @@ print(fis)
 thescript = 'run_scripts/cadence/metric.py'
 for fi in fis:
     procName = '.'.join(fi.split('/')[-1].split('.')[:-1])
-    print(procName,fi.split('/')[-1])
+    print(procName, fi.split('/')[-1])
     bb = BatchIt(processName=procName)
     params = {}
     params['fileName'] = fi.split('/')[-1]
     params['input_dir'] = input_dir
-    params['outName'] =  fi.split('/')[-1].replace('data',metric_name.split('Metric')[0].lower())
+    params['outName'] = fi.split(
+        '/')[-1].replace('data', metric_name.split('Metric')[0].lower())
     params['output_dir'] = output_dir
     params['metric'] = metric_name
     params['nproc'] = nproc
-    bb.add_batch(thescript,params)
+    params['type_data'] = type_data
+    bb.add_batch(thescript, params)
     bb.go_batch()
